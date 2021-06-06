a8b6b6cdf6842a2a0492cf3cf57344cd61f9ccf0
soroush.fathi <soroush8fathi@gmail.com>
2021-06-06 18:40:02 +0430

    fix reading bug and compute 4 operator

---
+++ src/input1.x
@@ -0,0 +1,12 @@
+int x = 10
+int  y =  30
+int z
+int w = 50
+int sum
+%%
+sum = x + y
+sum = sum + z
+sum = sum + w
+print sum
+x = sum / y
+print x
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,8 @@
 package main;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -14,23 +12,32 @@ public class Main {
     static HashMap<String,Float> floatValues = new HashMap<>();
 
     public static void main(String[] args) {
-        File file = new File("src\\input1.txt");
+        File file1 = new File("src\\input1.txt");
+        File file2 = new File("src\\input2.txt");
+        File xFile1 = new File("src\\input1.x");
         try {
-            readConfig(file);
+//            readConfig(file2);
+            read_compute(xFile1);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+            System.err.println("File Not Found!");
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
         }
-        System.out.println(intValues);
-        System.out.println(floatValues);
+//        System.out.println(intValues);
+//        System.out.println(floatValues);
     }
-    public static void readConfig(File file) throws FileNotFoundException , IllegalArgumentException{
+    public static void read_compute(File file) throws IOException, IllegalArgumentException,ArithmeticException{
+        FileReader fileReader = new FileReader(file);
         Scanner scanner = new Scanner(file);
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
-            String[] splited = line.split(" ");
-            System.out.println(Arrays.toString(splited));
-//            if (intValues.containsKey(splited[1]) || floatValues.containsKey(splited[1]))
-//                throw new IllegalArgumentException("Duplicated variable");
+            String[] splited = line.split("[\\s]+");
+//            System.out.println(Arrays.toString(splited));
+            if (line.trim().equals("%%"))
+                break;
+            if (intValues.containsKey(splited[1]) || floatValues.containsKey(splited[1]))
+                throw new IllegalArgumentException("Duplicated variable");
             if (splited[0].equals("int")) {
                 if (splited.length==4)
                     intValues.put(splited[1], Integer.parseInt(splited[3]));
@@ -42,8 +49,44 @@ public class Main {
                 else if (splited.length==2)
                     floatValues.put(splited[1],0.0f);
             }
-            if (line.trim().equals("%%"))
-                break;
+        }
+        while (scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            String[] splited = line.split("[\\s]+");
+//            System.out.println(Arrays.toString(splited));
+            if(splited[0].equals("print")){
+                if (intValues.containsKey(splited[1]))
+                    System.out.println(intValues.get(splited[1]));
+                else
+                    System.out.println(floatValues.get(splited[1]));
+            } else if (splited[3].equals("+")){
+                if (intValues.containsKey(splited[0]))
+                    intValues.computeIfPresent(splited[0],(k,v)->intValues.get(splited[2])+intValues.get(splited[4]));
+                else if (floatValues.containsKey(splited[0]))
+                    floatValues.computeIfPresent(splited[0],(k,v)->floatValues.get(splited[2])+ floatValues.get(splited[4]));
+            } else if (splited[3].equals("-")){
+                if (intValues.containsKey(splited[0]))
+                    intValues.computeIfPresent(splited[0],(k,v)->intValues.get(splited[2])-intValues.get(splited[4]));
+                else if (floatValues.containsKey(splited[0]))
+                    floatValues.computeIfPresent(splited[0],(k,v)->floatValues.get(splited[2])-floatValues.get(splited[4]));
+            } else if (splited[3].equals("*")){
+                if (intValues.containsKey(splited[0]))
+                    intValues.computeIfPresent(splited[0],(k,v)->intValues.get(splited[2])*intValues.get(splited[4]));
+                else if (floatValues.containsKey(splited[0]))
+                    floatValues.computeIfPresent(splited[0],(k,v)->floatValues.get(splited[2])*floatValues.get(splited[4]));
+            } else if (splited[3].equals("/")){
+                if (intValues.containsKey(splited[0]))
+                    if(intValues.get(splited[4])==0)
+                        throw new ArithmeticException();
+                    else
+                        intValues.computeIfPresent(splited[0],(k,v)->intValues.get(splited[2])/intValues.get(splited[4]));
+                else if (floatValues.containsKey(splited[0]))
+                    if(floatValues.get(splited[4])==0)
+                        throw new ArithmeticException();
+                    else
+                        floatValues.computeIfPresent(splited[0],(k,v)->floatValues.get(splited[2])/floatValues.get(splited[4]));
+            }
         }
     }
+
 }

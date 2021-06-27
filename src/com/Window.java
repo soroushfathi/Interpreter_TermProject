5a0c3de41bf824de7e1a4a6de5128367abf37304
soroush.fathi <soroush8fathi@gmail.com>
2021-06-27 22:08:33 +0430

    improve GUI

--- src/com/Window.java
+++ src/com/Window.java
@@ -2,6 +2,8 @@ package com;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -13,8 +15,12 @@ public class Window extends JFrame {
     JButton button;
     String string;
     JPanel panel;
+    JPanel southPanel;
+    JPanel eastPanel;
     public Window() throws HeadlessException {
         panel = new JPanel();
+        southPanel = new JPanel();
+        eastPanel = new JPanel();
         Image img = new ImageIcon("src\\assets\\images.jpg").getImage();
         setIconImage(img);
         try {
@@ -48,11 +54,54 @@ public class Window extends JFrame {
             }
         });
         string = txtArea.getText();
-        panel.add(button);
         scrollPane.setBounds(10,60,780,500);
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         panel.add(scrollPane);
-//        panel.add(txtArea);
+        JCheckBox boldCheckbox = new JCheckBox("Bold",false);
+        JCheckBox italicCheckbox = new JCheckBox("Italic",false);
+        ButtonGroup buttonGroup = new ButtonGroup();
+        JRadioButton small = new JRadioButton("small");
+        buttonGroup.add(small);
+        JRadioButton medium = new JRadioButton("medium");
+        buttonGroup.add(medium);
+        JRadioButton large = new JRadioButton("large");
+        buttonGroup.add(large);
+        ActionListener checkBoxListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int mode =0;
+                if (boldCheckbox.isSelected())
+                    mode += Font.BOLD;
+                if (italicCheckbox.isSelected())
+                    mode += Font.ITALIC;
+                txtArea.setFont(new Font(Font.SANS_SERIF,mode,14));
+            }
+        };
+        boldCheckbox.addActionListener(checkBoxListener);
+        italicCheckbox.addActionListener(checkBoxListener);
+        eastPanel.add(boldCheckbox);
+        eastPanel.add(italicCheckbox);
+        eastPanel.add(small);
+        eastPanel.add(medium);
+        eastPanel.add(large);
+        ActionListener sizeListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (small.isSelected())
+                    txtArea.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,10));
+                else if (medium.isSelected())
+                    txtArea.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,13));
+                else if (large.isSelected())
+                    txtArea.setFont(new Font(Font.SANS_SERIF,Font.PLAIN,17));
+            }
+        };
+        small.addActionListener(sizeListener);
+        medium.addActionListener(sizeListener);
+        large.addActionListener(sizeListener);
+        southPanel.add(button);
+
+        panel.add(southPanel,BorderLayout.SOUTH);
+        panel.add(eastPanel,BorderLayout.EAST);
         add(panel);
     }
 }
